e71021db52380e3144c8c733a34846e2323cac8b
acsdev <allancarneirosantos@gmail.com>
2018-08-11 15:10:23 -0300

    new mock data

--- nodejs/backend/index.js
+++ nodejs/backend/index.js
@@ -17,3 +17,29 @@ server.listen(3000, function(){ console.log('Listen on port 3000'); });
 server.get('/',function(req, res) {
 	res.status( 200 ).json( { 'status':'tested' } );
 });
+
+
+server.get('/listPairNumberString',function(req, res) {
+	var array = [];
+	for(var i = 0; i < 100; i++) {
+		var item  = {
+			id   : (i+1),
+			desc : Math.random().toString(36).replace(/[^a-z]+/g, '')
+		};	
+		array.push( item );
+	}
+	res.status( 200 ).json( array );
+});
+
+server.get('/listPairNumberDate',function(req, res) {
+	var array = [];
+	for(var i = 0; i < 100; i++) {
+		var item  = {
+			id   : (i+1),
+			desc : new Date(new Date().getTime() + (Math.random() * 1000000000)).toISOString()
+		};	
+		array.push( item );
+	}
+	res.status( 200 ).json( array );
+});
+

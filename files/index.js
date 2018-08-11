ad70fec166abc0f54c48d81b85675ddac1676b52
acsdev <allancarneirosantos@gmail.com>
2018-08-11 15:49:00 -0300

    just organization

--- files/index.js
+++ files/index.js
@@ -45,5 +45,43 @@
         }
     });
 
+    $(document).on('click','.tablinks', function() {
+        $('.tabcontent').hide();
+        $('.tablinks').removeClass('active');
+        //
+        var btn   = $(this);
+        var tab   = $('div#tab-view').children().get( $(btn).index() );
+        //
+        $(tab).fadeIn('slow', function() { $(btn).addClass('active') });
+    });
     
+    $('div#message-container').on('mousedown', function (e) {
+        $(this).addClass('divActiveMove').parents().on('mousemove', function (e) {
+            $('.divActiveMove').offset({
+                top: e.pageY - $('.divActiveMove').outerHeight() / 2,
+                left: e.pageX - $('.divActiveMove').outerWidth() / 2
+            }).on('mouseup', function () {
+                $(this).removeClass('divActiveMove');
+            });
+        });
+        return false;    
+    });
+
+    $(document).ready(function() {
+        var staticJsonSample = {
+            "_id": "5b6ef116157df345e9d7c77b",
+            "company": "IPLAX",
+            "email": "melbajoyce@iplax.com",
+            "phone": "+1 (904) 413-3834",
+            "address": "679 Kingston Avenue, Greenock",
+            "about": "Dolor sunt irure ",
+            "registered": "2015-12-28T03:36:34 +02:00",
+            "latitude": 33.819133,
+            "longitude": 111.044133
+        };
+        $('#ttaJ1,#ttaJ2').val( JSON.stringify( staticJsonSample, undefined, 2) );
+
+
+        $('.tab').children('a').get(1).click(); //START ON TAB 02
+    });
 })();
